36667931892940b3b394994e33b8b160aa6b4e6a
Yi Gao <yigao1983@gmail.com>
2016-12-28 01:02:37 -0600

    Add more files

--- code/HeteroLinearModel.py
+++ code/HeteroLinearModel.py
@@ -1,32 +1,54 @@
 import numpy as np
-import pandas as pd
-import sklearn.linear_model as lm
+import sklearn.metrics as metrics
 
 class HeteroLinearModel(object):
     
-    def __init__(self, csv_file):
+    def __init__(self, eta=1e-9):
         
-        df = pd.read_csv(csv_file)
-        
-        self.__X = df.x.values.reshape(len(df), 1)
-        self.__y = df.y.values
-        
-    @property
-    def X(self):
-        
-        return self.__X
+        self.__eta = eta
+        self.__a = 0
+        self.__b = 0
+        self.__c = 0
         
     @property
-    def y(self):
+    def intercept_(self):
+        return self.__b
         
-        return self.__y
+    @property
+    def coef_(self):
+        return self.__a
+    
+    @property
+    def multiplicity_(self):
+        return self.__c
         
-    def fit_ols(self):
+    def fit(self, X, y):
         
-        self.__ols = lm.LinearRegression(fit_intercept=True, normalize=True).fit(self.__X, self.__y)
+        a_11 = sum( X.ravel() / (np.abs(X.ravel()) + self.__eta) )
+        a_12 = sum( 1. / (np.abs(X.ravel()) + self.__eta) )
+        a_21 = sum( X.ravel()**2 / (np.abs(X.ravel()) + self.__eta) )
+        a_22 = a_11
+        
+        z_1 = sum( y / (np.abs(X.ravel()) + self.__eta) )
+        z_2 = sum( y * X.ravel() / (np.abs(X.ravel()) + self.__eta) )
+        
+        A = np.array([[a_11, a_12], [a_21, a_22]])
+        z = np.array([z_1, z_2])
+        
+        self.__a, self.__b = np.linalg.solve(A, z)
+        
+        y_pred = self.predict(X)
+        
+        c2 = np.mean( (y - y_pred)**2 / (np.abs(X) + self.__eta) )
+        
+        self.__c = np.sqrt(c2)
         
         return self
         
-    def predict(self):
+    def predict(self, X):
         
-        return self.__ols.predict(self.__X)
+        return self.__a * X.ravel() + self.__b
+        
+    def score(self, X, y):
+        
+        return metrics.r2_score(y, self.predict(X))
--- code/main.py
+++ code/main.py
@@ -1,25 +1,77 @@
 import glob
 import numpy as np
+import pandas as pd
+import sklearn.linear_model as lm
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import HeteroLinearModel as HLM
 
-def peek_pattern(csv_file_lst, power=0.5):
+def read_data(csv_file):
+    
+    try:
+        df = pd.read_csv(csv_file)
+        X = df.x.values.reshape(len(df), 1)
+        y = df.y.values
+        
+        return X, y
+        
+    except Exception as e:
+        print(e)
+
+def peek_pattern(csv_file_lst):
     
     markers = ("o", "s", "x", "d", "^")
     colors  = ("k", "b", "g", "r", "c")
     
+    ols_df = pd.DataFrame(columns=["File name", "a", "b"])
+    
     plt.figure()
+    X_all = []
+    err_all = []
     for idx, csv_file in enumerate(csv_file_lst):
-        hlm = HLM.HeteroLinearModel(csv_file).fit_ols()
-        plt.scatter(hlm.X, (hlm.y-hlm.predict()) / np.power(np.abs(hlm.X.ravel()), power), \
-                    color=colors[idx], marker=markers[idx], label=csv_file)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.legend(loc="best")
-    plt.show()    
+        X, y = read_data(csv_file)
+        ols = lm.LinearRegression().fit(X, y)
+        err = y - ols.predict(X)
+        
+        ols_df.loc[idx] = pd.Series([csv_file.split("/")[-1], ols.coef_.item(), ols.intercept_], \
+                                    index=ols_df.columns)
+        
+        plt.scatter(X, err/np.abs(X.ravel())**0.5, \
+                    marker=markers[idx], color=colors[idx], label="data_1_{}".format(idx+1))
+        
+        X_all.extend(X.ravel())
+        err_all.extend(err)
+    
+    ols_df.to_csv("ols.csv")
+    
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"OLS residual / $|x|^{1/2}$")
+    plt.legend(loc=2, frameon=False)
+    plt.savefig("peek_pattern.pdf", bbox_inches="tight")
+    
+    X_all   = np.array(X_all)
+    err_all = np.array(err_all)
+    XX_all  = np.vstack((np.abs(X_all), X_all**2))
+    
+    model = sm.OLS(err_all**2, sm.add_constant(XX_all.T))
+    result = model.fit()
+    
+    print(result.summary())
+    
+def evaluate_params(csv_files):
+    
+    hlm_df = pd.DataFrame(columns=["File name", "a", "b"])
+    
+    for idx, csv_file in enumerate(csv_file_lst):
+        X, y = read_data(csv_file)
+        hlm = HLM.HeteroLinearModel().fit(X, y)
+        hlm_df.loc[idx] = pd.Series([csv_file.split("/")[-1], hlm.coef_, hlm.intercept_], \
+                                    index=hlm_df.columns)
+    
+    hlm_df.to_csv("hlm.csv")
     
 if __name__ == "__main__":
     
     csv_file_lst = sorted([csv for csv in glob.glob("../*.csv")])
-    
-    peek_pattern(csv_file_lst, power=0.5)
+    peek_pattern(csv_file_lst)
+    evaluate_params(csv_file_lst)
